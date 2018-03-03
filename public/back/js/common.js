7b8ec8eae0f52ce9376857f3b0a2fc1591768466
doujintao <291585325@qq.com>
2018-03-03 21:50:27 +0800

    eachests使用

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,4 +1,6 @@
 $(function(){
+
+  //NProgress  进度条js
   //禁用进度环
   NProgress.configure({
     showSpinner: false
@@ -16,4 +18,21 @@ $(function(){
     }, 3000);
   });
 
+  //首页导航菜单js
+  //二级菜单的显示与隐藏
+  //思路： 找到二级分类的a标签
+  //prev()获取元素紧邻的前一个同胞元素
+  $('.second').prev().on('click',function(){
+    //slideToggle() 方法通过使用滑动效果（高度变化）来切换元素的可见状态
+    $(this).next().slideToggle();
+  })
+
+  //首页menu缩进事件
+  //找到icon_menu注册点击事件
+  $(".icon_menu").on("click",function(){
+    //让侧边栏隐藏
+    $('.lt_aside').toggleClass("now");
+    //让main的padding-left:0
+    $('.lt_main').toggleClass("now");
+  })
 })
