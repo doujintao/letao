74b79d170e020f4c0911c8b25aeabc2b7284d873
doujintao <291585325@qq.com>
2018-03-04 23:01:40 +0800

    二级分类菜单渲染及分页

---
+++ public/back/js/second.js
@@ -0,0 +1,36 @@
+/**
+ * Created by Administrator on 2018/3/4.
+ */
+$(function(){
+  //渲染二级分类列表以及分页
+  var page = 1;
+  var pageSize = 3;
+
+  var render = function (){
+    $.ajax({
+      type:'GET',
+      url:"/category/querySecondCategoryPaging",
+      data:{
+        page:page,
+        pageSize:pageSize
+      },
+      success:function(info){
+        //console.log(info);
+        $('tbody').html(template('tpl',info));
+
+        //渲染分页
+        $('#paginator').bootstrapPaginator({
+          bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
+          currentPage:page,//当前页
+          totalPages:Math.ceil(info.total/info.size),//总页数
+          onPageClicked:function(event, originalEvent, type,p){
+            page=p;
+            render();
+          }
+        })
+      }
+    })
+  }
+
+  render();
+})
